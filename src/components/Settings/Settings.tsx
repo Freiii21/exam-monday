7af52f8f29eadbed506a995c1c536dbc65997ffe
Freiii21 <mrfreiii@gmail.com>
2021-11-16 00:14:01 +0300

    #42 Adding settings to counter. Part 3

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -8,12 +8,16 @@ export const Settings = (props:SettingsPropsType) => {
     return (
         <div className={s.common}>
             <div className={s.row}>
-                <div>max value</div>
-                <div><input type={'number'} value={'5'}/></div>
+                <div>max value:</div>
+                <div>
+                    <input type={'number'} value={'5'} className={s.input}/>
+                </div>
             </div>
             <div className={s.row}>
-                <div>start value</div>
-                <div><input type={'number'} value={'0'}/></div>
+                <div>start value:</div>
+                <div>
+                    <input type={'number'} value={'0'} className={s.input}/>
+                </div>
             </div>
         </div>
     )
